a47e9833f948011831c785c79be31bfb8af42daf
SlartiBartFast-art <JekHamond@tutanota.com>
2022-02-09 20:32:41 +0500

    8. Валидация моделей в Spring REST [#504801] Уровень : 3. МидлКатегория : 3.4. SpringТопик : 3.4.8. Rest #02

--- src/main/java/ru/job4j/entity/Person.java
+++ src/main/java/ru/job4j/entity/Person.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-import javax.validation.constraints.Min;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
